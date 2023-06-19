b6953aae62992f898bf2685cc8ffe3f27bada48d
Lamartune <fatih.tun.2002@gmail.com>
2023-06-19 11:53:08 +0300

    Done Alert Example

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { Children } from "react";
+import { Children, useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        Hello <span>Worldd</span> World!
-      </Alert>
-      <Button onClick={() => console.log("clicked")}>My Button</Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert </Alert>
+      )}
+      <Button onClick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
